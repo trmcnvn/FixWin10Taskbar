dfbeac7bc092ece21abd7e5ea85f302bdd00e63c
Thomas McNiven <trmcniven@gmail.com>
2013-10-28 02:04:26 +1000

    Make the application actually work =/

--- FixWin81Taskbar.Injector/Main.cpp
+++ FixWin81Taskbar.Injector/Main.cpp
@@ -3,7 +3,26 @@
 #include <codecvt>
 #include <locale>
 
-#include "Injector.hpp"
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+void Inject(const std::wstring& className, const std::wstring& source) {
+	DWORD procId;
+	GetWindowThreadProcessId(FindWindowW(className.c_str(), NULL), &procId);
+	auto procHandle = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_CREATE_THREAD |
+		PROCESS_VM_WRITE | PROCESS_VM_READ | PROCESS_VM_OPERATION, FALSE, procId);
+
+	auto addr = VirtualAllocEx(procHandle, NULL, source.size() * 2, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+	auto ret = WriteProcessMemory(procHandle, addr, source.data(), source.size() * 2, NULL);
+
+	auto libaddr = GetProcAddress(GetModuleHandleW(L"kernel32.dll"), "LoadLibraryW");
+	auto thread = CreateRemoteThread(procHandle, NULL, NULL, reinterpret_cast<LPTHREAD_START_ROUTINE>(libaddr), addr, NULL, NULL);
+
+	WaitForSingleObject(thread, INFINITE);
+	VirtualFreeEx(procHandle, addr, source.size() * 2, MEM_RELEASE);
+	CloseHandle(thread);
+	CloseHandle(procHandle);
+}
 
 
 int main(int argc, char** argv) {
@@ -23,7 +42,5 @@ int main(int argc, char** argv) {
 	appPath.append(L"\\FixWin81Taskbar.Module.dll");
 
 	// Inject into explorer.exe
-	auto inj = new Injector();
-	inj->Inject(L"Shell_SecondaryTrayWnd", appPath);
-	delete inj;
+	Inject(L"Shell_SecondaryTrayWnd", appPath);
 }
--- FixWin81Taskbar.Module/DllMain.cpp
+++ FixWin81Taskbar.Module/DllMain.cpp
@@ -1,25 +1,39 @@
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
+#include <cstdio>
+#include <iostream>
 
-LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
-	LPCWPRETSTRUCT msg = reinterpret_cast<LPCWPRETSTRUCT>(lParam);
+LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
+	if (nCode == HC_ACTION && wParam == WM_LBUTTONUP && lParam) {
+		MOUSEHOOKSTRUCT* msg = reinterpret_cast<MOUSEHOOKSTRUCT*>(lParam);
 
-	if (msg->message == WM_LBUTTONUP) {
-		auto hWnd = FindWindowW(L"Shell_TrayWnd", NULL);
-		ShowWindow(hWnd, SW_SHOW);
+		HWND hWnd = FindWindowW(L"Shell_SecondaryTrayWnd", NULL);
+		if (msg->hwnd == hWnd) {
+			SetForegroundWindow(FindWindowW(L"Shell_TrayWnd", NULL));
+		}
 	}
 
 	return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
+DWORD WINAPI dwThread(LPVOID lpParam) {
+	auto msgHook = SetWindowsHookExW(WH_MOUSE, MouseProc, reinterpret_cast<HINSTANCE>(lpParam), 0);
+	
+	MSG messages;
+	while (GetMessageW(&messages, NULL, 0, 0)) {
+		TranslateMessage(&messages);
+		DispatchMessageW(&messages);
+	}
+
+	UnhookWindowsHookEx(msgHook);
+	return TRUE;
+}
+
 BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpvReserved) {
-	HHOOK msgHook = nullptr;
+	if (fdwReason == DLL_PROCESS_ATTACH) {
 
-	if (fdwReason == DLL_PROCESS_ATTACH)
-		msgHook = SetWindowsHookExW(WH_CALLWNDPROCRET, HookProc, hInstDLL, 0);
-	else if (fdwReason == DLL_PROCESS_DETACH)
-		if (msgHook != nullptr)
-			UnhookWindowsHookEx(msgHook);
-
+		auto thread = CreateThread(NULL, NULL, dwThread, hInstDLL, NULL, NULL);
+		CloseHandle(thread);
+	}
 	return TRUE;
 }
